62b6232b265dc0e0e396ebe865237f515257f9bc
RomanPalna <roma.palna@gmail.com>
2022-12-04 13:58:05 +0200

    redeploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const getMenu = require("./syrve-api");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
+// kjsdgfjhsdbfsdHKBfskdJn
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
